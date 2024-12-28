170abe66300931cd5de5ee13c49bb56f5b9461f9
Gatcha <help@gatcha.us>
2024-12-28 14:50:57 -0500

    update link-address

--- src/page/home/link-address.tsx
+++ src/page/home/link-address.tsx
@@ -3,12 +3,22 @@ import { useNavigate, NavLink } from 'react-router';
 import { observer } from 'mobx-react-lite';
 import { Paper, Stack, Box, Button, Typography } from '@mui/material';
 import { styled } from '@mui/system';
+import CStyled from 'styled-components';
 
 import Color from '@tool/color';
 
 interface IProps {}
 
 const DStack = styled(Stack)({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '1200px',
+  padding: '2rem',
+  marginTop: '3rem',
+  boxSizing: 'border-box',
+  backgroundColor: Color.black(0.1),
+});
+const EStack = styled(Stack)({
   display: 'flex',
   flexDirection: 'row',
   marginTop: '2rem',
@@ -17,6 +27,20 @@ const DTypography = styled(Typography)({
   margin: '0 1rem',
   textDecoration: 'underline',
 });
+const Item = CStyled.section`
+  width: 15rem;
+  height: 8rem;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 2rem;
+  box-sizing: border-box;
+  cursor: pointer;
+  border: 4px solid ${Color.black(0)};
+  background-color: ${Color.black(0.3)};
+
+  &:hover { border: 4px solid ${Color.black(1)} }
+`;
 
 const Tangram = (props: IProps) => {
   const navi = useNavigate();
@@ -27,24 +51,27 @@ const Tangram = (props: IProps) => {
 
   return (
     <DStack>
-      <NavLink to="/gallery">
-        <DTypography variant="body1">To Gallery</DTypography>
-      </NavLink>
-      <NavLink to="/stores?storeName=tangram">
-        <DTypography variant="body1">To Flushing NY</DTypography>
-      </NavLink>
-      <NavLink to="/parties-info">
-        <DTypography variant="body1">To Parties-Info</DTypography>
-      </NavLink>
-      <NavLink to="/stores?storeName=orlando-fl">
-        <DTypography variant="body1">To Orlando FL</DTypography>
-      </NavLink>
-      <NavLink to="/stores?storeName=saugus-ma">
-        <DTypography variant="body1">To Saugus MA</DTypography>
-      </NavLink>
-      <NavLink to="/stores?storeName=edison-nj">
-        <DTypography variant="body1">To Edison NJ</DTypography>
-      </NavLink>
+      <Typography variant="h6">Available Link</Typography>
+      <EStack>
+        <Item onClick={() => navi('gallery')}>
+          <DTypography variant="body1">To Gallery</DTypography>
+        </Item>
+        <Item onClick={() => navi('stores?storeName=tangram')}>
+          <DTypography variant="body1">To Flushing NY</DTypography>
+        </Item>
+        <Item onClick={() => navi('parties-info')}>
+          <DTypography variant="body1">To Parties-Info</DTypography>
+        </Item>
+        <Item onClick={() => navi('stores?storeName=orlando-fl')}>
+          <DTypography variant="body1">To Orlando FL</DTypography>
+        </Item>
+        <Item onClick={() => navi('stores?storeName=saugus-ma')}>
+          <DTypography variant="body1">To Saugus MA</DTypography>
+        </Item>
+        <Item onClick={() => navi('stores?storeName=edison-nj')}>
+          <DTypography variant="body1">To Edison NJ</DTypography>
+        </Item>
+      </EStack>
     </DStack>
   );
 };
